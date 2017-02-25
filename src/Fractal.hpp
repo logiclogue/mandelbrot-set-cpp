29911bbffc0de6c24307eec5b8b6beafc5c2f23d
logiclogue <jordanpaullord@hotmail.co.uk>
2017-02-25 22:04:07 +0000

    Adds Fractal header

---
+++ src/Fractal.hpp
@@ -0,0 +1,14 @@
+#ifndef FRACTAL_H
+#define FRACTAL_H
+
+#include "types.hpp"
+
+namespace Fractals
+{
+    class Fractal {
+    public:
+        virtual type_complex get(type_complex values) = 0;
+    }
+}
+
+#endif
