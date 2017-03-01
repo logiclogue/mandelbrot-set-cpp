5f6d31abe5f1be8f021058172ff12aec557104c0
logiclogue <jordanpaullord@hotmail.co.uk>
2017-03-01 13:16:40 +0000

    Adds Camera header

---
+++ src/Camera.hpp
@@ -0,0 +1,16 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include "types.hpp"
+
+namespace Frames
+{
+    class Camera {
+    public:
+        type_float x;
+        type_float y;
+        type_float zoom;
+    }
+}
+
+#endif
