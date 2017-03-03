2e87c0406ecfccfe3a1d77b1a8c397f6bcb83156
logiclogue <jordanpaullord@hotmail.co.uk>
2017-03-03 07:54:05 +0000

    Adds method translate_coords_to_complex

--- src/Translator.cpp
+++ src/Translator.cpp
@@ -17,9 +17,17 @@ namespace Translators
 
     type_complex Translator::translate_coords_to_complex(int x, int y)
     {
-        type_float complex_x = x;
-        type_float complex_y = y;
-        type_complex output(complex_x, complex_y);
+        type_float x_ratio = (x / frame->width) - 0.5;
+        type_float y_ratio = (y / frame->height) - 0.5;
+
+        type_float x_zoom = camera->zoom;
+        type_float y_zoom = camera->zoom * (frame->height / frame->width);
+
+        type_float x_complex_non_shifted = x_ratio * x_zoom;
+        type_float y_complex_non_shifted = y_ratio * y_zoom;
+
+        type_complex non_shifted(x_complex_non_shifted, y_complex_non_shifted);
+        type_complex output = camera->focus + non_shifted;
 
         return output;
     }
