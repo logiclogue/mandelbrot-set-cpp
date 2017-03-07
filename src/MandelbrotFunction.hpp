85482986c241b98ff3858c5433f497965d4c7cdf
logiclogue <jordanpaullord@hotmail.co.uk>
2017-03-07 07:33:42 +0000

    Tweaks MandelbrotFunction to remove private

--- src/MandelbrotFunction.hpp
+++ src/MandelbrotFunction.hpp
@@ -6,8 +6,8 @@
 namespace Functions
 {
     class MandelbrotFunction : public Function {
-    private:
         type_complex z;
+
     public:
         MandelbrotFunction();
         type_complex get(type_complex z, type_complex c);
