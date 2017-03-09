4e52e9fd5c463b549e03ec0db28579f9a15e7286
logiclogue <jordanpaullord@hotmail.co.uk>
2017-03-09 07:19:34 +0000

    Adds PngDrawer header file

---
+++ src/PngDrawer.hpp
@@ -0,0 +1,23 @@
+#ifndef PNG_DRAWER_HPP
+#define PNG_DRAWER_HPP
+
+#include "Drawer.hpp"
+#include "Translator.hpp"
+#include "Iterator.hpp"
+
+namespace Drawers
+{
+    class PngDrawer : public Drawer {
+        Iterators::Iterator *iterator;
+        Translators::Translator *translator;
+
+    public:
+        PngDrawer(
+            Iterators::Iterator *iterator,
+            Translators::Translator *translator,
+            char *path);
+        void draw();
+    }
+}
+
+#endif
