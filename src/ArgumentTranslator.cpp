f9ae55b4fc7847c0ad41019f3afa848b00e863b4
logiclogue <jordanpaullord@hotmail.co.uk>
2017-03-29 08:41:45 +0100

    Refactors ArgumentTranslator

--- src/ArgumentTranslator.cpp
+++ src/ArgumentTranslator.cpp
@@ -11,53 +11,9 @@ namespace Translators
         int argc, char *argv[])
     {
         DrawerFactoryModel *model = new DrawerFactoryModel();
-        int i;
-        char *flag, *value;
-        char null_string[] = "";
 
         _set_default_model(model);
-
-        for (i = 0; i < argc; i += 1) {
-            flag = argv[i];
-
-            if (i == argc - 1) {
-                value = null_string;
-            } else {
-                value = argv[i + 1];
-            }
-
-            if (!strcmp(flag, "--x-coord")) {
-                model->x = atof(value);
-            } else if (!strcmp(flag, "--y-coord")) {
-                model->y = atof(value);
-            } else if (!strcmp(flag, "--width")) {
-                model->width = atoi(value);
-            } else if (!strcmp(flag, "--height")) {
-                model->height = atoi(value);
-            } else if (!strcmp(flag, "--ratio")) {
-                model->ratio = atof(value);
-            } else if (!strcmp(flag, "--zoom")) {
-                model->zoom = atof(value);
-            } else if (!strcmp(flag, "--iterations")) {
-                model->iterations = atoi(value);
-            } else if (!strcmp(flag, "--julia-r")) {
-                model->is_julia_set = true;
-                model->julia_r = atof(value);
-            } else if (!strcmp(flag, "--julia-i")) {
-                model->is_julia_set = true;
-                model->julia_i = atof(value);
-            } else if (!strcmp(flag, "--multi-r")) {
-                model->is_multibrot_set = true;
-                model->multi_r = atof(value);
-            } else if (!strcmp(flag, "--multi-i")) {
-                model->is_multibrot_set = true;
-                model->multi_i = atof(value);
-            } else if (!strcmp(flag, "--help")) {
-                model->is_help_text = true;
-            } else if (!strcmp(flag, "--bitmap")) {
-                model->bitmap = true;
-            }
-        }
+        _populate_model_from_arguments(model, argc, argv);
 
         return model;
     }
@@ -78,5 +34,61 @@ namespace Translators
         model->multi_i = 0;
         model->is_multibrot_set = false;
         model->bitmap = false;
+    }
+
+    void ArgumentTranslator::_populate_model_from_arguments(
+        Models::DrawerFactoryModel *model, int argc, char *argv[])
+    {
+        int i;
+        char *flag, *value;
+        char null_string[] = "";
+
+        for (i = 0; i < argc; i += 1) {
+            flag = argv[i];
+
+            if (i == argc - 1) {
+                value = null_string;
+            } else {
+                value = argv[i + 1];
+            }
+
+            _set_model_variable_from_argument(model, flag, value);
+        }
+    }
+
+    void ArgumentTranslator::_set_model_variable_from_argument(
+        Models::DrawerFactoryModel *model, char *flag, char *value)
+    {
+        if (!strcmp(flag, "--x-coord")) {
+            model->x = atof(value);
+        } else if (!strcmp(flag, "--y-coord")) {
+            model->y = atof(value);
+        } else if (!strcmp(flag, "--width")) {
+            model->width = atoi(value);
+        } else if (!strcmp(flag, "--height")) {
+            model->height = atoi(value);
+        } else if (!strcmp(flag, "--ratio")) {
+            model->ratio = atof(value);
+        } else if (!strcmp(flag, "--zoom")) {
+            model->zoom = atof(value);
+        } else if (!strcmp(flag, "--iterations")) {
+            model->iterations = atoi(value);
+        } else if (!strcmp(flag, "--julia-r")) {
+            model->is_julia_set = true;
+            model->julia_r = atof(value);
+        } else if (!strcmp(flag, "--julia-i")) {
+            model->is_julia_set = true;
+            model->julia_i = atof(value);
+        } else if (!strcmp(flag, "--multi-r")) {
+            model->is_multibrot_set = true;
+            model->multi_r = atof(value);
+        } else if (!strcmp(flag, "--multi-i")) {
+            model->is_multibrot_set = true;
+            model->multi_i = atof(value);
+        } else if (!strcmp(flag, "--help")) {
+            model->is_help_text = true;
+        } else if (!strcmp(flag, "--bitmap")) {
+            model->bitmap = true;
+        }
     }
 }
